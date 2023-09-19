d2edb3864d55937f032886f3cef83e895039089e
JamethDavid <112991287+JamethDavid@users.noreply.github.com>
2023-09-19 11:34:47 -0500

    revizar

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app-footer',
+    template:'<h1>dedo</h1>'
+})
+export class FooterComponent{
+    public autor:any ={nombre:'Jameth',apllido:'Melgarejo'};
+
+
+}
+
+
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Comment } from "@angular/compiler";
+import { Component } from "@angular/core";
+@Component({
+    selector:'app-header',
+    template:'./header.Component.html'
+
+})
+
+export class HeaderComponent{
+
+}
